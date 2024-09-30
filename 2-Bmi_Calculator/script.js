9d6fc4af7777680808fc10eab0c91ade381e3f1a
Sameer Ahmed <samxyzdev@gmail.com>
2024-09-30 15:00:13 +0530

    complete bmi calculator

---
+++ 2-Bmi_Calculator/script.js
@@ -0,0 +1,24 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const height = parseInt(document.querySelector("#height").value);
+  const weight = parseInt(document.querySelector("#weight").value);
+  const results = document.querySelector("#results");
+
+  if (height === "" || height < 0 || isNaN(height)) {
+    results.innerHTML = `Please give a child height ${height}`;
+  } else if (weight === "" || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please give a child weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    if (bmi < 18.6) {
+      results.innerHTML = `Under Weight (BMI: ${bmi})`;
+    } else if (bmi >= 18.6 && bmi < 24.9) {
+      results.innerHTML = `Normal Range (BMI: ${bmi})`;
+    } else {
+      results.innerHTML = `Overweight (BMI: ${bmi})`;
+    }
+  }
+});
